8365be91026ad1254ff6e85ad2f5f734c20e8110
plb97 <plb97@users.noreply.github.com>
2017-06-14 09:38:11 +0200

    petits changements

--- cont.go
+++ cont.go
@@ -10,49 +10,43 @@ import (
 )
 // http://www-irma.u-strasbg.fr/~richez/ressources/recherche/memoire_fractions_continues.pdf
 // 'Cond_red' retourne :
-// le tableau des elements('n' au plus depend de 'prec') de la suite continue
-// le tableau des elements('n' au plus depend de 'prec') des fractions continues correspondantes
-// la valeur approchee a la precision 'prec' au plus (depend de la valeur 'n') de la partie fractionnaire de 'f'
-// la partie entiere de 'f'
+// le tableau des elements de la suite continue (reduite (suite finie) de la fraction continue)
+// le tableau des elements des fractions continues correspondantes
 // la valeur majorant l'erreur d'approximation
-// 'e' est la reduite (suite finie) de la fraction continue
 //
 // p[0] = a[0]	p[1] = a[1]*a[0] + 1	et pour n>=2	p[n] = a[n]*p[n-1] + p[n-2]
 // q[0] = 1	q[1] = a[1]		et pour n>=2	q[n] = a[n]*q[n-1] + q[n-2]
 // REMARQUE : en posant p[-1] = 1 et q[-1] = 0 on obtient
 //		p[1] = a[1]*p[0] + p[-1] = a[1]*p[0] + 1
 //		q[1] = a[1]*q[0] + q[-1] = a[1]*q[0] + 0
-// TODO l'utilisation de 'n' et 'prec' comme criteres d'arret pourrait etre a revoir ('prec' devrait rester seul)
-func Cont_red(f float64, n int, prec float64) ([]int, []*Fract_t, float64, int, float64) {
-	if 0 >= prec {panic("Precision invalide")}
-	if 0 >= n {panic("Parametre invalide")}
-	var e, r = make([]int,0,n), make([]*Fract_t,0,n) // 'e' tableau des elements de la suite continue, 'r' tableau des fractions correspondantes
+func Cont_red(f float64, prec float64) ([]int, []*Fract_t, float64) {
+	if 1e-18 > prec {panic("Precision invalide < '1e-18'")} // '1e-18' est empirique
+	var r, e = make([]int,0), make([]*Fract_t,0) // 'e' tableau des elements de la suite continue, 'r' tableau des fractions correspondantes
 	a, b := fqa.Ent(f) // 'a' partie entiere, 'b' reste fractionnaire 0 <= 'b' < 1
-	c, d := a, 1e0
+	d := 1e0
 	p, ap := Creer(a,1), Creer(1,0) // a/1 , 1/0 (+Inf)
-	e, r = append(e,a), append(r,p) // initialiser les tableaux
-	arret := func(p,ap *Fract_t, b float64, i int) bool {
+	r, e = append(r,a), append(e,p) // initialiser les tableaux
+	arret := func(p,ap *Fract_t, b float64) bool {
 		d = 1 / float64(p.d*p.d)
-		ok := i >= n || // 'n' est atteint
-			fqa.Egal_f(b, 0, prec) || // 'b' est nul (ou presque)
+		ok := fqa.Egal_f(b, 0, prec) || // 'b' est nul (ou presque)
 			fqa.Egal_f(d, 0, prec) // l'ecart |'p.n/p.d' - 'f'| < 1 / p.d*p.d est nul (ou presque)
 		return ok
 	}
-	for i := 1; !arret(p,ap,b,i); i++ { // boucler
+	for !arret(p,ap,b) { // boucler
 		a, b = fqa.Ent(1 / b) // 'a' partie entiere, 'b' reste fractionnaire 0 <= 'b' < 1
 		p, ap = Creer(a*p.n+ap.n,a*p.d+ap.d), p // 'p' nouvelle fraction, 'ap' fraction precedente
-		e, r = append(e,a), append(r,p) // ajouter les elements aux tableaux
+		r, e = append(r,a), append(e,p) // ajouter les elements aux tableaux
 	}
-	return e, r, b, c, d // retourner les resultats
+	return r, e, d // retourner les resultats
 }
-// 'Cont' retourne un tableau (d'au plus 'n' elements') des fractions continues qui approchent 'f'
-func Cont(f float64, n int, prec float64) ([]*Fract_t) {
-	_, r, _, _, _ := Cont_red(f, n, prec)
+// 'Cont' retourne un tableau des fractions continues qui approchent 'f'
+func Cont(f float64, prec float64) ([]*Fract_t) {
+	_, r, _ := Cont_red(f, prec)
 	return r
 }
 // Cont_elmts' retourne le tableau des au plus 'n' elements de la suite continue approchant 'f'
-func Cont_elmts(f float64, n int, prec float64) ([]int) {
-	e, _, _, _, _ := Cont_red(f, n, prec)
+func Cont_elmts(f float64, prec float64) ([]int) {
+	e, _, _ := Cont_red(f, prec)
 	return e
 }
 
--- cont_test.go
+++ cont_test.go
@@ -22,41 +22,44 @@ func Test_cont(t *testing.T) {
 	}
 	for _, v := range test_cont {
 		attendu := v.r
-		n := len(attendu)
-		p := 1e-20
-		obtenu := Cont(v.f,n,p)
-		if n < len(obtenu) {
-			t.Errorf(test+"(%f,%d): attendu n %v >= obtenu %v",v.f,n,n, len(obtenu))
-		} else {
-			for i := range obtenu {
-				if !attendu[i].Egal(obtenu[i]) {
-					t.Errorf(test+"(%f,%d): [%d] attendu %v, obtenu %v",v.f,n, i, attendu[i], obtenu[i])
-				}
+		p := 1e-5
+		obtenu := Cont(v.f,p)
+		n := len(obtenu)
+		if n > len(attendu) {
+			n = len(attendu)
+		}
+		for i := 0; i < n; i++ {
+			if !attendu[i].Egal(obtenu[i]) {
+				t.Errorf(test+"(%f,%d): [%d] attendu %v, obtenu %v",v.f,n, i, attendu[i], obtenu[i])
 			}
 		}
-		p = 1e-9
-		obtenu = Cont(v.f,n,p)
-		if n < len(obtenu) {
-			t.Errorf(test+"(%f,%d): attendu n %v >= obtenu %v",v.f,n,n, len(obtenu))
-		} else {
-			for i := 0; i < len(obtenu); i++ {
-				if !attendu[i].Egal(obtenu[i]) {
-					t.Errorf(test+"(%f,%d): [%d] attendu %v, obtenu %v",v.f,n, i, attendu[i], obtenu[i])
-				}
+		p = 1e-5
+		obtenu = Cont(v.f,p)
+		n = len(obtenu)
+		if n > len(attendu) {
+			n = len(attendu)
+		}
+		for i := 0; i < n; i++ {
+			if !attendu[i].Egal(obtenu[i]) {
+				t.Errorf(test+"(%f,%d): [%d] attendu %v, obtenu %v",v.f,n, i, attendu[i], obtenu[i])
 			}
 		}
-	}
+		}
 }
 
 func Test_elmts(t *testing.T) {
 	test := "elmts"
 	f := math.Pi
 	fc := []int{3,7,15,1,292}
-	n := 5
-	p := 1e-7
 	{
-		lo := Cont_elmts(f,n,p)
-		for i, attendu := range fc {
+		p := 1e-18
+		lo := Cont_elmts(f,p)
+		n := len(lo)
+		if n > len(fc) {
+			n = len(fc)
+		}
+		for i := 0; i < n; i++ {
+			attendu := fc[i]
 			obtenu := lo[i]
 			if attendu != obtenu {
 				t.Errorf(test+" : attendu %v != obtenu %v", attendu, obtenu)
@@ -66,43 +69,64 @@ func Test_elmts(t *testing.T) {
 
 }
 
+func Test_prec(t *testing.T) {
+	test := "prec"
+	f := math.Pi
+	{
+		ctr := 0
+		defer func() {
+			if r := recover(); r != nil {
+				ctr++
+				attendu := "Precision invalide < '1e-18'"
+				obtenu, ok := r.(string)
+				if !ok {
+					t.Errorf(test+": attendu %v != obtenu %v\n", !ok, ok)
+				}
+				if attendu != obtenu {
+					t.Errorf(test+": attendu %v != obtenu %v\n", attendu, obtenu)
+				}
+			}
+		}()
+		p := 1e-19
+		Cont_elmts(f,p)
+		if 1 != ctr {
+			t.Errorf(test+": attendu %v != obtenu %v\n", 1, ctr)
+		}
+	}
+
+}
+
 func Test_red(t *testing.T) {
 	test := "red"
 	f := math.Phi
 	li := []int{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-	n := len(li) // suffisamment grand
-	p := 1e-6 // precision souhaitee (pas trop grande)
-	e,r,b,c,d := Cont_red(f,n,p)
+	p := 1e-8 // precision souhaitee (pas trop grande)
+	r, e, d := Cont_red(f,p)
 	{
-		if len(li) < len(e) {
-			t.Errorf(test+" : attendu %v >= obtenu %v", len(li), len(e))
+		n := len(r)
+		if n > len(li) {
+			n = len(li)
 		}
-		for i, obtenu := range e {
+		for i := 0; i < i; i++ {
 			attendu := li[i]
+			obtenu := r[i]
 			if attendu != obtenu {
 				t.Errorf(test+" : attendu %v != obtenu %v", attendu, obtenu)
 			}
 		}
 	}
 	{
-		attendu := f - 1
-		obtenu := b
+		attendu := f
+		obtenu := e[len(e)-1].Valeur()
 		if !fqa.Egal_f(obtenu,attendu,p) {
-			t.Errorf(test+" : attendu %v != obtenu %v prec=%v ecart=%v", attendu, obtenu,p,obtenu - attendu)
+			t.Errorf(test+" : attendu %v != obtenu %v prec=%v ecart=%v", attendu, obtenu, p, obtenu - attendu)
 		}
 	}
 	{
 		attendu := f
-		obtenu := r[len(r)-1].Valeur()
+		obtenu := e[len(e)-1].Valeur()
 		if !fqa.Egal_f(obtenu,attendu,p) {
 			t.Errorf(test+" : attendu %v != obtenu %v prec=%v ecart=%v", attendu, obtenu,p,obtenu - attendu)
-		}
-	}
-	{
-		attendu := 1
-		obtenu := c
-		if attendu != obtenu {
-			t.Errorf(test + " : attendu %v != obtenu %v", attendu, obtenu)
 		}
 	}
 	{
